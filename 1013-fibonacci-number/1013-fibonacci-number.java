68a66d4187e469d2e665070207475e5e81226eee
sharmashivam755 <sharmashivam72479@gmail.com>
2025-11-11 23:18:27 +0530

    Time: 0 ms (100%), Space: 41.8 MB (12.11%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,16 +1,21 @@
 public class Solution {
     
     public static int fib(int n) {
-        if (n == 0)
+        if (n == 0) {
             return 0; 
-        else if (n == 1)
+         } else if (n == 1) {
             return 1; 
-        else
-            return fib(n - 1) + fib(n - 2); 
+    }
+    int firstTerm = 0;
+    int secondTerm = 1;
+
+    for(int i = 1;i<=n;i++){
+        int thirdTerm = firstTerm + secondTerm ;
+        firstTerm = secondTerm ;
+        secondTerm = thirdTerm ;
     }
 
-    public static void main(String[] args) {
-        int n = 3; 
-        System.out.println("F(" + n + ") = " + fib(n));
-    }
+    return firstTerm ;
+   }
 }
+ 
