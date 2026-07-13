9a33d8adf7f2a0c847fe3ddfb137523098c13138
sharmashivam755 <sharmashivam72479@gmail.com>
2026-07-13 15:06:55 +0530

    Time: 3 ms (87.34%), Space: 43.4 MB (62.49%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        Integer[] res = new Integer[heights.length];
+
+        for (int i = 0; i < heights.length;i++) {
+            res[i] = heights[i];
+        }
+        Arrays.sort(res);
+        int count = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != res[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
